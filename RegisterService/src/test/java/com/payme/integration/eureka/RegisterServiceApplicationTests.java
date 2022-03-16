b4d639af57f6814f3802c4c44055032f4f8ad470
Erick Meza <emeza@ALIGNET.COM>
2022-03-16 11:39:16 -0500

    Eureka Server

---
+++ RegisterService/src/test/java/com/payme/integration/eureka/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payme.integration.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
